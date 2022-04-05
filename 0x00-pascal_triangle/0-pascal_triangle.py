e919d1481a3b3930cdd3b42c0f2fe43d07f6c055
Derrick Ampire <ampire90@gmail.com>
2022-04-05 19:06:39 +0000

    First files added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Returns a list of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+# This directory contains the file for the Project 0x00. Pascal's Triangle
