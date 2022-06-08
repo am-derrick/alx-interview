4e1c6a7f353de79996b2132dbba63bdbe2ad3554
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-06-08 12:12:18 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid """
+    total_perimeter = 0
+
+    for i, row in enumerate(grid):
+        for j, element in enumerate(row):
+            if (element == 0):
+                continue
+
+            if (j != 0 and row[j - 1] == 0):
+                total_perimeter += 1
+            if (j == 0):
+                total_perimeter += 1
+
+            if (j != len(row) - 1 and row[j + 1] == 0):
+                total_perimeter += 1
+            if (j == len(row) - 1):
+                total_perimeter += 1
+
+            if (i != 0 and grid[i - 1][j] == 0):
+                total_perimeter += 1
+            if (i == 0):
+                total_perimeter += 1
+
+            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
+                total_perimeter += 1
+            if (i == len(grid) - 1):
+                total_perimeter += 1
+
+    return total_perimeter
